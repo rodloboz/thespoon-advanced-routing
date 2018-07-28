bed42c8896a1d6a572e6b1a9c54e3e9de8fae0ac
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-07-28 10:17:55 +0800

    added reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      # GET /restaurants/top
+      get :top
+    end
+    member do
+      # GET /restaurants/42/chef
+      get :chef
+    end
+    resources :reviews, only: [:new, :create]
+  end
+  # GET /best-restaurant
+  get 'best-restaurant', to: 'restaurants#best'
+  root to: 'restaurants#index'
 end
